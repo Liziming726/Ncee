4521d3f30d5d1a7e1d1e10238c3bbcff4fd789e0
Liziming726 <873884635@qq.com>
2023-05-13 22:27:08 +0800

    update

---
+++ app/components/Login/Login.js
@@ -0,0 +1,91 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import {
+  Button,
+  Dialog,
+  DialogContent,
+  DialogTitle,
+  Input,
+  TextField,
+} from "@mui/material";
+import styles from "./Login.module.css";
+import axios from "axios";
+
+export default function Login() {
+  const [open, setOpen] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post(
+        "https://example.com/login",
+        {
+          username: username,
+          password: password,
+        }
+      );
+      localStorage.setItem("user", JSON.stringify(response.data));
+      handleClose();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <Button onClick={handleClickOpen}>
+        <p>Sign in</p>
+      </Button>
+
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>
+          <Image
+            src="/vercel.svg"
+            alt="Vercel Logo"
+            className={styles.vercelLogo}
+            width={400}
+            height={24}
+            priority
+          />
+        </DialogTitle>
+        <DialogContent>
+          <p className={styles.title}>Email address</p>
+          <Input
+            className={styles.ipt}
+            type="username"
+            value={username}
+            onChange={handleUsernameChange}
+          />
+          <p className={styles.title}>Password</p>
+          <Input
+            className={styles.ipt}
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+          <Button className={styles.submit} onClick={handleSubmit}>
+            Sign in
+          </Button>
+          {/* <a href="">Forgot password ?</a> */}
+        </DialogContent>
+      </Dialog>
+    </div>
+  );
+}
